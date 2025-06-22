4f2533719f1abd021b61e8e3c85649bd01ef2baa
H. Nguyen <vagabon@gmail.com>
2025-06-22 10:45:46 +0700

    Revert "Add comment for DocumentIcon in InitiativeDetailPage"
    This reverts commit e4a8d76be2663a0735d204839215cad108e4fa15.

--- src/pages/InitiativeDetailPage.tsx
+++ src/pages/InitiativeDetailPage.tsx
@@ -11,7 +11,6 @@ const DocumentIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>
 );
 
-// Icon cho các tài liệu
 const CheckCircleIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-green-500 mr-3 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
